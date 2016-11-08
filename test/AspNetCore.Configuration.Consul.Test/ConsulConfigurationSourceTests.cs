8b2d5c06506595a7f9535499bcd52f85b76c8184
Choc <matthew.thornton13@gmail.com>
2016-11-08 08:06:59 +0000

    Create test project and add tests for ConsulConfigurationSource

---
+++ test/AspNetCore.Configuration.Consul.Test/ConsulConfigurationSourceTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace Chocolate.AspNetCore.Configuration.Consul
+{
+    [TestFixture]
+    internal sealed class ConsulConfigurationSourceTests
+    {
+        [Test]
+        public void ShouldHaveJsonConfgurationParserByDefault()
+        {
+            var source = new ConsulConfigurationSource();
+
+            Assert.That(source.Parser, Is.TypeOf<JsonConfigurationParser>());
+        }
+
+        [Test]
+        public void ShouldSetOptionalToFalseByDefault()
+        {
+            var source = new ConsulConfigurationSource();
+
+            Assert.That(source.Optional, Is.False);
+        }
+
+        [Test]
+        public void ShouldSetReloadOnChangeToFalseByDefault()
+        {
+            var source = new ConsulConfigurationSource();
+
+            Assert.That(source.ReloadOnChange, Is.False);
+        }
+    }
+}
