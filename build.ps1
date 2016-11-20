2160d3c7779158bdc9adac616907bf084d0fcc76
Choc <matthew.thornton13@gmail.com>
2016-11-20 21:32:35 +0000

    Fix errors in appveyor build script

--- build.ps1
+++ build.ps1
@@ -27,5 +27,5 @@ cd src\AspNetCore.Configuration.Consul
 exec { & dotnet gitversion }
 cd ..\..\
 exec { & dotnet build src\*\project.json test\*\project.json --configuration Release }
-exec { & dotnet test --no-build --configuration Release -f netcoreapp1.0 test\*\project.json }
+exec { & dotnet test --no-build --configuration Release -f netcoreapp1.0 test\AspNetCore.Configuration.Consul.Test\project.json }
 exec { & dotnet pack --no-build src\*\project.json --configuration Release }
