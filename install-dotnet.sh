1752a5121e6cd95429c4dcc7b6fd651bb4edb903
Choc <matthew.thornton13@gmail.com>
2016-11-20 19:41:15 +0000

    Remove build steps from dotnet install script

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -46,13 +46,3 @@ fi
 
 # We just use KoreBuild to install the dotnet cli
 $dotnetInstallFile
-
-# Then we do a custom build pipeline that include versioning
-dotnet restore
-cd src/AspNetCore.Configuration.Consul
-dotnet gitversion
-cd ../../
-dotnet build src/*/project.json test/*/project.json --configuration Release
-dotnet test --no-build --configuration Release -f netcoreapp1.0 test/*/project.json
-dotnet pack --no-build src/*/project.json --configuration Release
-nuget push src/AspNetCore.Configuration.Consul/bin/Release/*.nupkg -ApiKey 40a11e65-c7f8-47cb-bfb9-c86ccd5e8234 -NonInteractive -Verbosity Detailed
