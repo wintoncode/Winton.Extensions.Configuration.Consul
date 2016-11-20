f87328217738e675f14cd9a4dbb6669b772ff6aa
Choc <matthew.thornton13@gmail.com>
2016-11-20 21:21:44 +0000

    Tidy up appveyor build scripts

--- build.ps1
+++ build.ps1
@@ -1,8 +1,7 @@
-dotnet restore
+exec { & dotnet restore }
 cd src\AspNetCore.Configuration.Consul
-dotnet gitversion
+exec { & dotnet gitversion }
 cd ..\..\
-dotnet build src\*\project.json test\*\project.json --configuration Release
-dotnet test --no-build --configuration Release -f netcoreapp1.0 test\*\project.json
-dotnet pack --no-build src\*\project.json --configuration Release
-nuget push src/AspNetCore.Configuration.Consul/bin/Release/*.nupkg -ApiKey 40a11e65-c7f8-47cb-bfb9-c86ccd5e8234 -NonInteractive -Verbosity Detailed
+exec { & dotnet build src\*\project.json test\*\project.json --configuration Release }
+exec { & dotnet test --no-build --configuration Release -f netcoreapp1.0 test\*\project.json }
+exec { & dotnet pack --no-build src\*\project.json --configuration Release }
