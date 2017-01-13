feaf311739e61177d044de13ab72e39001871b6b
Matt Thornton <matthew.thornton13@gmail.com>
2017-01-13 23:01:34 +0000

    Travis PR Test (#1)
    * Update build.sh and travis.yml script to skip versioning for pull requests as git in detached head state.
    
    * Remove redundant travis environment variable
    
    * Fix error in travis.yml
    
    * Update check for travis pull request to check for false

--- build.sh
+++ build.sh
@@ -1,11 +1,34 @@
 set -e
+
+while getopts v option
+do
+    case "${option}"
+    in
+            v) VERSION_AND_PUBLISH="true";;
+    esac
+done
+
+# Restore packages
 dotnet restore
-cd src/Winton.Extensions.Configuration.Consul
-dotnet gitversion
-cd ../../
+
+# Set version numbers
+if [[ $VERSION_AND_PUBLISH ]]; then
+    echo "Versioning..."
+    (cd src/Winton.Extensions.Configuration.Consul && dotnet gitversion)
+else
+    echo "WARN: Skipping versioning."
+fi
+
+# Build
 dotnet build src/Winton.Extensions.Configuration.Consul/project.json test/Winton.Extensions.Configuration.Consul.Test/project.json --configuration Release
+
+# Unit Test
 dotnet test --no-build --configuration Release -f netcoreapp1.0 test/Winton.Extensions.Configuration.Consul.Test/project.json
+
+# Integration test
 if hash docker 2>/dev/null; then
     ./test/Website/IntegrationTests/run.sh
 fi
+
+# Package
 dotnet pack --no-build src/*/project.json --configuration Release
