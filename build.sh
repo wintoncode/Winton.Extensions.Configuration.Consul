b1839e69fb0404123bf6f2cece87a723de01c507
Matthew Thornton <m.thornton@winton.com>
2018-07-16 21:33:47 +0100

    Fix runtime error when using lib in netframework app.

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-
 set -e
 
 # Restore packages
 dotnet restore
 
 # Build
-dotnet build --configuration Release
+dotnet build src/Winton.Extensions.Configuration.Consul --configuration Release --framework netstandard1.3
+dotnet build test/Winton.Extensions.Configuration.Consul.Test --configuration Release --framework netcoreapp2.0
+dotnet build test/Website --configuration Release --framework netcoreapp1.1
 
 # Unit Test
 dotnet test test/Winton.Extensions.Configuration.Consul.Test/ --no-build --configuration Release
