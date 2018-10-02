f38724939b35255a143913bcdf3d4c30ea7578f3
Matt Thornton <matthew.thornton13@gmail.com>
2018-10-02 15:27:53 +0100

    Fix bug JsonPrimitiveVisitor not respecting the culture of JsonSerializer.

--- test/Winton.Extensions.Configuration.Consul.Test/Parsers/Json/JsonPrimitiveVisitorTests.cs
+++ test/Winton.Extensions.Configuration.Consul.Test/Parsers/Json/JsonPrimitiveVisitorTests.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 using FluentAssertions;
 using Newtonsoft.Json.Linq;
 using Xunit;
@@ -76,6 +78,21 @@ namespace Winton.Extensions.Configuration.Consul.Parsers.Json
                     }
                 }
             };
+
+            [Fact]
+            private void ShouldConvertPrimitivesToStringUsingJsonSerializerCulture()
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+
+                IEnumerable<KeyValuePair<string, string>> primitives =
+                    _visitor.VisitJObject(new JObject(new JProperty("Test", new JValue(1.5))));
+
+                primitives.Should().BeEquivalentTo(
+                    new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("Test", "1.5")
+                    });
+            }
 
             [Theory]
             [MemberData(nameof(TestCases))]
