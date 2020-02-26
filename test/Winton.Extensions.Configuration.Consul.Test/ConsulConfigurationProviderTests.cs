aceae35c2d4258ec303775398d6da13d8d93e9dc
jamarino <jamarino94@gmail.com>
2020-02-26 09:21:56 +0000

    Made ConsulConfigurationProvider.Dispose idempotent (#98)

--- test/Winton.Extensions.Configuration.Consul.Test/ConsulConfigurationProviderTests.cs
+++ test/Winton.Extensions.Configuration.Consul.Test/ConsulConfigurationProviderTests.cs
@@ -112,6 +112,23 @@ namespace Winton.Extensions.Configuration.Consul
                     kv => kv.List("Test", It.IsAny<QueryOptions>(), It.IsAny<CancellationToken>()),
                     Times.Between(expectedKvCalls, expectedKvCalls + 1, Range.Inclusive));
             }
+
+            [Fact]
+            private void ShouldNotThrowOnMultipleDisposeCalls()
+            {
+                _source.ReloadOnChange = true;
+                _source.Optional = true;
+                _kvEndpoint
+                    .Setup(kv => kv.List("Test", It.IsAny<QueryOptions>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(new QueryResult<KVPair[]> { StatusCode = HttpStatusCode.OK });
+
+                _provider.Load();
+                _provider.Dispose();
+
+                var secondDispose = _provider.Invoking(p => p.Dispose());
+
+                secondDispose.Should().NotThrow();
+            }
         }
 
         public sealed class DoNotReloadOnChange : ConsulConfigurationProviderTests
