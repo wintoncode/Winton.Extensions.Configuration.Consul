fd87286e253f40be18145c273d9ddbccc9e80729
Matt Thornton <matthew.thornton13@gmail.com>
2020-09-14 14:15:02 +0100

    Fix issue with forward slashes in JSON keys being converted to colons. (#114)

--- test/Winton.Extensions.Configuration.Consul.Test/Parsers/JsonConfigurationParserTests.cs
+++ test/Winton.Extensions.Configuration.Consul.Test/Parsers/JsonConfigurationParserTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using FluentAssertions;
@@ -16,14 +17,33 @@ namespace Winton.Extensions.Configuration.Consul.Parsers
 
         public sealed class Parse : JsonConfigurationParserTests
         {
+            public static IEnumerable<object[]> TestCases => new List<object[]>
+            {
+                new object[]
+                {
+                    "{\"Key\": \"Value\"}",
+                    new Dictionary<string, string> { { "Key", "Value" } }
+                },
+                new object[]
+                {
+                    "{\"parent\": {\"child\": \"Value\"} }",
+                    new Dictionary<string, string?> { { "parent", null }, { "parent:child", "Value" } }
+                },
+                new object[]
+                {
+                    "{\"Key/WithSlash\": \"Value\"}",
+                    new Dictionary<string, string> { { "Key/WithSlash", "Value" } }
+                }
+            };
+
             [Theory]
-            [InlineData("{\"Key\": \"Value\"}", "Key", "Value")]
-            [InlineData("{\"parent\": {\"child\": \"Value\"} }", "parent:child", "Value")]
-            private void ShouldParseSimpleJsonFromStream(string json, string key, string expectedValue)
+            [MemberData(nameof(TestCases))]
+            private void ShouldParseSimpleJsonFromStream(string json, IDictionary<string, string?> expected)
             {
                 using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
                 var result = _parser.Parse(stream);
-                result[key].Should().Be(expectedValue);
+
+                result.Should().BeEquivalentTo(expected);
             }
         }
     }
