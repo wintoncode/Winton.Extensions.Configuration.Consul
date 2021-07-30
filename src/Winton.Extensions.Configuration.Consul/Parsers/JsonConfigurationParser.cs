0ba6037e061a08526a686aed9555c794dacaef41
kklldog <kklldog@qq.com>
2021-07-31 01:23:34 +0800

    Use Microsoft's JsonStreamConfigurationProvider class to parse Json stream into a dictionary .

--- src/Winton.Extensions.Configuration.Consul/Parsers/JsonConfigurationParser.cs
+++ src/Winton.Extensions.Configuration.Consul/Parsers/JsonConfigurationParser.cs
@@ -3,8 +3,7 @@
 
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace Winton.Extensions.Configuration.Consul.Parsers
 {
@@ -14,14 +13,29 @@ namespace Winton.Extensions.Configuration.Consul.Parsers
     /// </summary>
     public sealed class JsonConfigurationParser : IConfigurationParser
     {
-        /// <inheritdoc />
+        /// <summary>
+        /// Parse the <see cref="Stream" /> into a dictionary.
+        /// </summary>
+        /// <param name="stream">The stream to parse.</param>
+        /// <returns>A dictionary representing the configuration in a flattened form.</returns>
         public IDictionary<string, string> Parse(Stream stream)
         {
-            return new ConfigurationBuilder()
-                .AddJsonStream(stream)
-                .Build()
-                .AsEnumerable()
-                .ToDictionary(pair => pair.Key, pair => pair.Value);
+            return JsonStreamParser.Parse(stream);
+        }
+
+        private sealed class JsonStreamParser : JsonStreamConfigurationProvider
+        {
+            private JsonStreamParser(JsonStreamConfigurationSource source)
+                : base(source)
+            {
+            }
+
+            internal static IDictionary<string, string> Parse(Stream stream)
+            {
+                var provider = new JsonStreamParser(new JsonStreamConfigurationSource { Stream = stream });
+                provider.Load();
+                return provider.Data;
+            }
         }
     }
 }
